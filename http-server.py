2d941f7dcea0c825d4a8478fe6d33a9b98577b4a
41223219 <41223219@nfu.edu.tw>
2025-03-25 02:48:42 +0000

    新增SSH的操作影片

--- http-server.py
+++ http-server.py
@@ -1,22 +1,24 @@
 import os
-import subprocess
 import threading
-import http.server, ssl
+import http.server
+import ssl
 
 def domake():
-    # build directory
-    #os.chdir("./../")
-    server_address = ('localhost', 8444)
+    # 設定伺服器地址 (改為 8000)
+    server_address = ('localhost', 8000)
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-    httpd.socket = ssl.wrap_socket(httpd.socket,
-                                   server_side=True,
-                                   certfile='./localhost.crt',
-                                   keyfile='./localhost.key',
-                                   ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+    # 創建 SSL 上下文
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile='./localhost.crt', keyfile='./localhost.key')
+
+    # 使用 context 來包裝 socket
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
     print(os.getcwd())
-    print("8444 https server started")
+    print("8000 https server started")
     httpd.serve_forever()
 
-# 利用執行緒執行 https 伺服器
+# 利用執行緒啟動 HTTPS 伺服器
 make = threading.Thread(target=domake)
 make.start()
